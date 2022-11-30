ff59807d29455764dac1dd3296da996d628cf327
tobiboth <115711501+tobiboth@users.noreply.github.com>
2022-11-30 17:32:41 +0100

    aufgabe7_nach feedback

--- Aufgabe_7/js/eu-scriptt.js
+++ Aufgabe_7/js/eu-scriptt.js
@@ -1,61 +1,49 @@
 //Deutschland
-const DE = "Deutschland";
 const bevölkerungDE2022 = 83240000;
 const bevölkerungDE2006 = 82310000;
 //Frankreich
-const FR = "Frankreich";
 const bevölkerungFR2022 = 67840000;
 const bevölkerungFR2006 = 63620000;
 //Italien
-const IT = "Italien";
 const bevölkerungIT2022 = 58980000;
 const bevölkerungIT2006 = 58140000;
 //Portugal
-const P = "Portugal";
 const bevölkerungP2022 = 10350000;
 const bevölkerungP2006 = 10520000;
 //EU
 const bevölkerungEU = 447700000;
-//Deutschland
-function germany() {
-    document.querySelector("#people22").innerHTML = (bevölkerungDE2022 / 1000000) + "Mio.";
-    document.querySelector("#text").innerHTML = "Bevölkerung in Deutschland 2022";
-    document.querySelector("#relative").innerHTML = (bevölkerungDE2022 / bevölkerungEU * 100) + "%";
-    document.querySelector("#rate").innerHTML = ((1 - bevölkerungDE2006 / bevölkerungDE2022) * 100) + "%";
-    document.querySelector("#people06").innerHTML = ((bevölkerungDE2022 - bevölkerungDE2006) / 1000000) + "Mio.";
-    document.querySelector(".chart").style.height = (bevölkerungDE2022 / bevölkerungEU * 100) + "%";
+//allgemein
+var nation;
+var population22;
+var population06;
+function population(nation, population22, population06) {
+    document.querySelector("#people22").innerHTML = (population22 / 1000000).toFixed(2) + "Mio.";
+    document.querySelector("#text").innerHTML = "Bevölkerung in " + nation + " 2022";
+    document.querySelector("#relative").innerHTML = (population22 / bevölkerungEU * 100).toFixed(2) + "%";
+    document.querySelector("#rate").innerHTML = ((1 - population06 / population22) * 100).toFixed(2) + "%";
+    document.querySelector("#people06").innerHTML = ((population22 - population06) / 1000000) + "Mio.";
+    document.querySelector(".chart").style.height = (population22 / bevölkerungEU * 100) + "%";
 }
-document.querySelector(".germany").addEventListener('click', germany);
-//Frankreich
-function france() {
-    document.querySelector("#people22").innerHTML = (bevölkerungFR2022 / 1000000) + "Mio.";
-    document.querySelector("#text").innerHTML = "Bevölkerung in Frankreich 2022";
-    document.querySelector("#relative").innerHTML = (bevölkerungFR2022 / bevölkerungEU * 100) + "%";
-    document.querySelector("#rate").innerHTML = ((1 - bevölkerungFR2006 / bevölkerungFR2022) * 100) + "%";
-    document.querySelector("#people06").innerHTML = ((bevölkerungFR2022 - bevölkerungFR2006) / 1000000) + "Mio.";
-    document.querySelector(".chart").style.height = (bevölkerungFR2022 / bevölkerungEU * 100) + "%";
+document.querySelector(".germany").addEventListener('click', function () {
+    population("Deutschland", bevölkerungDE2022, bevölkerungDE2006);
+});
+document.querySelector(".france").addEventListener('click', function () {
+    population("Frankreich", bevölkerungFR2022, bevölkerungFR2006);
+});
+document.querySelector(".italy").addEventListener('click', function () {
+    population("Italien", bevölkerungIT2022, bevölkerungIT2006);
+});
+document.querySelector(".portugal").addEventListener('click', function () {
+    population("Portugal", bevölkerungP2022, bevölkerungP2006);
+});
+/**function eu (nation, bevölkerungEU): void {
+    document.querySelector(".stars") as HTMLImageElement, (bevölkerungEU/ 1000000).toFixed(2) + "Mio.";
 }
-document.querySelector(".france").addEventListener('click', france);
-//Italien
-function italy() {
-    document.querySelector("#people22").innerHTML = (bevölkerungIT2022 / 1000000) + "Mio.";
-    document.querySelector("#text").innerHTML = "Bevölkerung in Italien 2022";
-    document.querySelector("#relative").innerHTML = (bevölkerungIT2022 / bevölkerungEU * 100) + "%";
-    document.querySelector("#rate").innerHTML = ((1 - bevölkerungIT2006 / bevölkerungIT2022) * 100) + "%";
-    document.querySelector("#people06").innerHTML = ((bevölkerungIT2022 - bevölkerungIT2006) / 1000000) + "Mio.";
-    document.querySelector(".chart").style.height = (bevölkerungIT2022 / bevölkerungEU * 100) + "%";
-}
-document.querySelector(".italy").addEventListener('click', italy);
-//Portugal
-function portugal() {
-    document.querySelector("#people22").innerHTML = (bevölkerungP2022 / 1000000) + "Mio.";
-    document.querySelector("#text").innerHTML = "Bevölkerung in Portugal 2022";
-    document.querySelector("#relative").innerHTML = (bevölkerungP2022 / bevölkerungEU * 100) + "%";
-    document.querySelector("#rate").innerHTML = ((1 - bevölkerungP2022 / bevölkerungP2006) * 100) + "%";
-    document.querySelector("#people06").innerHTML = ((bevölkerungP2022 - bevölkerungP2006) / 1000000) + "Mio.";
-    document.querySelector(".chart").style.height = (bevölkerungP2022 / bevölkerungEU * 100) + "%";
-}
-document.querySelector(".portugal").addEventListener('click', portugal);
+
+document.querySelector(".stars").addEventListener('click', function(){
+    eu ("EU-Bevölkerungszahl: ", bevölkerungEU);
+})
+*/
 /**
  * int main(){
  *
@@ -83,7 +71,7 @@ document.querySelector(".portugal").addEventListener('click', portugal);
 /**
  * document.querySelector("#test").addEventListener("click",myfunction () {
  * myfunction(3, 8);
- * };
+ * }
  *
  * function myfunction(number1,number2) {
  * const result = number1 + number2;
