54824c14148873d55efdaa33dcc63f44c86a2add
tobiboth <115711501+tobiboth@users.noreply.github.com>
2023-01-11 17:47:16 +0100

    aufgabe_11

--- aufgabe_11/js/todoo.js
+++ aufgabe_11/js/todoo.js
@@ -15,52 +15,24 @@ window.addEventListener("load", function () {
     addButtonDOMElement = document.querySelector("#addButton");
     todosDOMElement = document.querySelector("#todos");
     counterDOMElement = document.querySelector("#counter");
-    /**
-     * Jetzt da der DOM verfügbar ist kann auch ein Event-Listener
-     * auf den AddToDo Button gesetzt werden.
-     */
     addButtonDOMElement.addEventListener("click", addTodo);
-    /**
-     * Initial soll einmal die Liste an bereit definierten ToDos
-     * aus den Arrays in den DOM gezeichnet werden.
-     */
     drawListToDOM();
 });
 function drawListToDOM() {
     todosDOMElement.innerHTML = "";
-    // das ToDo-Array durchlaufen (iterieren) und Todo für Todo in den DOM schreiben
     for (let index = 0; index < todopoint.length; index++) {
-        /**
-         * Neues DIV-Element erstellen (würde auch mit innerHTML = "<div class='todo'></div>" gehen,
-         * die Objekt-Instansierung ist aber übersichtlicher)
-         */
         let todo = document.createElement("div");
         todo.classList.add("todo");
-        /**
-         * Jedes Todo besteht aus etwas Markup, also aus HTML-Elementen
-         * wie der Check-Anzeige, dem ToDo-Text und dem Mülleimer
-         *
-         * Einfachheitshalber werden hier alle HTML-Elemente für ein ToDo
-         * nicht DOM-Objekt-weise (wie oben, mit createElement), sondern als eine lange
-         * HTML-Zeichenkette erstellt. An manchen Stellen der Zeichenkette wird
-         * ein Wert einer Variablen benötigt (bspw. für die CSS Klasse oder für den ToDo-Text),
-         * hier muss die Zeichenkette unterbrochen werden.
-         */
         todo.innerHTML = "<span class='check " + todopoint[index].Checked + "'><i class='fas fa-check'></i></span>"
             + todopoint[index].Text +
             "<span class='trash fas fa-trash-alt'></span>";
-        // Zuweisen der Event-Listener für den Check- und den Trash-Button
         todo.querySelector(".check").addEventListener("click", function () {
-            // hier wird der Index, also die aktuelle Stelle im Array dieses ToDos,
-            // übergeben, damit an der entsprechenden Stelle im Array der Wert geändert werden kann.
+            / übergeben, damit an der entsprechenden Stelle im Array der Wert geändert werden kann.;
             toggleCheckState(index);
         });
         todo.querySelector(".trash").addEventListener("click", function () {
-            // hier wird der Index, also die aktuelle Stelle im Array dieses ToDos,
-            // übergeben, damit die entsprechende Stelle im Array gelöscht werden kann.
             deleteTodo(index);
         });
-        // Bis hier hin wurde das neue Todo "zusammengebaut", jetzt wird es in den DOM gerendert.
         todosDOMElement.appendChild(todo);
     }
     updateCounter();
--- aufgabe_11/js/todoo.ts
+++ aufgabe_11/js/todoo.ts
@@ -15,7 +15,7 @@ interface Listpoint {
     Text: string;
     Checked: boolean;
 }
-let todopoint: Listpoint[] =[{
+let todopoint: Listpoint[] = [{
     Text: "test1",
     Checked: true
 },
@@ -29,66 +29,42 @@ var addButtonDOMElement: HTMLElement;
 var todosDOMElement: HTMLElement;
 var counterDOMElement: HTMLElement;
 
-window.addEventListener("load", function(): void {
+window.addEventListener("load", function (): void {
 
     inputDOMElement = document.querySelector("#inputfield");
     addButtonDOMElement = document.querySelector("#addButton");
     todosDOMElement = document.querySelector("#todos");
     counterDOMElement = document.querySelector("#counter");
 
-    /**
-     * Jetzt da der DOM verfügbar ist kann auch ein Event-Listener
-     * auf den AddToDo Button gesetzt werden.
-     */
+
     addButtonDOMElement.addEventListener("click", addTodo);
 
-    /**
-     * Initial soll einmal die Liste an bereit definierten ToDos
-     * aus den Arrays in den DOM gezeichnet werden.
-     */
+
     drawListToDOM();
 });
 
 function drawListToDOM(): void {
     todosDOMElement.innerHTML = "";
 
-    // das ToDo-Array durchlaufen (iterieren) und Todo für Todo in den DOM schreiben
     for (let index: number = 0; index < todopoint.length; index++) {
 
-        /**
-         * Neues DIV-Element erstellen (würde auch mit innerHTML = "<div class='todo'></div>" gehen, 
-         * die Objekt-Instansierung ist aber übersichtlicher)
-         */
+
         let todo: HTMLElement = document.createElement("div");
         todo.classList.add("todo");
 
-        /**
-         * Jedes Todo besteht aus etwas Markup, also aus HTML-Elementen
-         * wie der Check-Anzeige, dem ToDo-Text und dem Mülleimer
-         * 
-         * Einfachheitshalber werden hier alle HTML-Elemente für ein ToDo
-         * nicht DOM-Objekt-weise (wie oben, mit createElement), sondern als eine lange
-         * HTML-Zeichenkette erstellt. An manchen Stellen der Zeichenkette wird
-         * ein Wert einer Variablen benötigt (bspw. für die CSS Klasse oder für den ToDo-Text),
-         * hier muss die Zeichenkette unterbrochen werden.
-         */
-        todo.innerHTML =  "<span class='check "+ todopoint[index].Checked +"'><i class='fas fa-check'></i></span>"
-                            + todopoint[index].Text +
-                            "<span class='trash fas fa-trash-alt'></span>";
 
-        // Zuweisen der Event-Listener für den Check- und den Trash-Button
-        todo.querySelector(".check").addEventListener("click", function(): void {
-            // hier wird der Index, also die aktuelle Stelle im Array dieses ToDos,
-            // übergeben, damit an der entsprechenden Stelle im Array der Wert geändert werden kann.
+        todo.innerHTML = "<span class='check " + todopoint[index].Checked + "'><i class='fas fa-check'></i></span>"
+            + todopoint[index].Text +
+            "<span class='trash fas fa-trash-alt'></span>";
+
+        todo.querySelector(".check").addEventListener("click", function (): void {
+            / übergeben, damit an der entsprechenden Stelle im Array der Wert geändert werden kann.
             toggleCheckState(index);
         });
-        todo.querySelector(".trash").addEventListener("click", function(): void {
-            // hier wird der Index, also die aktuelle Stelle im Array dieses ToDos,
-            // übergeben, damit die entsprechende Stelle im Array gelöscht werden kann.
+        todo.querySelector(".trash").addEventListener("click", function (): void {
             deleteTodo(index);
         });
 
-        // Bis hier hin wurde das neue Todo "zusammengebaut", jetzt wird es in den DOM gerendert.
         todosDOMElement.appendChild(todo);
     }
     updateCounter();
@@ -103,12 +79,12 @@ function updateCounter(): void {
 function addTodo(): void {
     if (inputDOMElement.value != "") {
 
-        let newtask={
+        let newtask = {
             Text: inputDOMElement.value,
             Checked: false
         }
         todopoint.unshift(newtask);
-        
+
         inputDOMElement.value = "";
 
         drawListToDOM();
@@ -123,8 +99,8 @@ function toggleCheckState(index: number): void {
 
 
 function deleteTodo(index: number): void {
-   
+
     todopoint.splice(index, 1);
-   
+
     drawListToDOM();
 }
